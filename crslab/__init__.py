b5b227db56f1a8a06d0edc8f2cdb1f8c5d786da4
wxl <wxl1999@foxmail.com>
2021-03-23 09:27:05 +0800

    FIX: gpu option

--- crslab/__init__.py
+++ crslab/__init__.py
@@ -1,8 +1,1 @@
 __version__ = '0.0.1'
-
-try:
-    import torch
-    import torch_geometric
-except Exception:
-    raise Exception('Please install PyTorch and PyTorch Geometric manually first.\n' +
-                    'View CRSLab GitHub page for more information: https://github.com/RUCAIBox/CRSLab')
--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
+try:
+    import torch
+    import torch_geometric
+except Exception:
+    raise Exception('Please install PyTorch and PyTorch Geometric manually first.\n' +
+                    'View CRSLab GitHub page for more information: https://github.com/RUCAIBox/CRSLab')
 
 setup_requires = []
 
