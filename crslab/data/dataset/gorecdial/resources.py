81261fa859ac2efe7ad46eccf8724d635636cdb1
Xiaolei Wang <wxl1999@foxmail.com>
2023-04-25 20:14:54 +0800

    FIX: download links for dataset gorecdial

--- crslab/data/dataset/gorecdial/resources.py
+++ crslab/data/dataset/gorecdial/resources.py
@@ -12,11 +12,11 @@ from crslab.download import DownloadableFile
 
 resources = {
     'nltk': {
-        'version': '0.3',
+        'version': '0.31',
         'file': DownloadableFile(
-            'https://pkueducn-my.sharepoint.com/:u:/g/personal/franciszhou_pku_edu_cn/ESM_Wc7sbAlOgZWo_6lOx34B6mboskdpNdB7FLuyXUET2A?download=1',
+            'https://pkueducn-my.sharepoint.com/:u:/g/personal/franciszhou_pku_edu_cn/ESIqjwAg0ItAu7WGfukIt3cBXjzi7AZ9L_lcbFT1aS1qYQ?download=1',
             'gorecdial_nltk.zip',
-            '7e523f7ca90bb32ee8f2471ac5736717c45b20822c63bd958d0546de0a9cd863',
+            '58cd368f8f83c0c8555becc314a0017990545f71aefb7e93a52581c97d1b8e9b',
         ),
         'special_token_idx': {
             'pad': 0,
@@ -29,11 +29,11 @@ resources = {
         },
     },
     'bert': {
-        'version': '0.3',
+        'version': '0.31',
         'file': DownloadableFile(
-            'https://pkueducn-my.sharepoint.com/:u:/g/personal/franciszhou_pku_edu_cn/EcTG05imCYpFiBarVfnsAfkBVsbq1iPw23CYcp9kYE9X4g?download=1',
+            'https://pkueducn-my.sharepoint.com/:u:/g/personal/franciszhou_pku_edu_cn/Ed1HT8gzvRpDosVT83BEj5QBnzKpjR3Zbf5u49yyWP-k6Q?download=1',
             'gorecdial_bert.zip',
-            'fc7aff18504f750d8974d90f2941a01ff22cc054283124936b778ba91f03554f'
+            '4fa10c3fe8ba538af0f393c99892739fcb376d832616aa7028334c594b3fec10'
         ),
         'special_token_idx': {
             'pad': 0,
@@ -48,11 +48,11 @@ resources = {
         }
     },
     'gpt2': {
-        'version': '0.3',
+        'version': '0.31',
         'file': DownloadableFile(
-            'https://pkueducn-my.sharepoint.com/:u:/g/personal/franciszhou_pku_edu_cn/Edg4_nbKA49HnQPcd65gPdoBALPADQd4V5qVqOrUub2m9w?download=1',
+            'https://pkueducn-my.sharepoint.com/:u:/g/personal/franciszhou_pku_edu_cn/EUJOHmX8v79DkZMq0x5r9d4B0UJlfw85v-VdciwKfAhpng?download=1',
             'gorecdial_gpt2.zip',
-            '7234138dcc27ed00bdac95da4096cd435023c229d227fa494d2bd7a653a492a9'
+            '44a15637e014b2e6628102ff654e1aef7ec1cbfa34b7ada1a03f294f72ddd4b1'
         ),
         'special_token_idx': {
             'pad': 0,
